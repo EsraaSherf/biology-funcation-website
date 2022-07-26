b47d2fbb55d415b359ccdb88e0f0ce7d4cc2faad
EsraaSherf <106784079+EsraaSherf@users.noreply.github.com>
2022-07-26 22:13:56 +0200

    Biological Background

---
+++ form1.php
@@ -0,0 +1,87 @@
+﻿<!DOCTYPE html>
+
+<html lang="en" xmlns="http://www.w3.org/1999/xhtml">
+<head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link rel="stylesheet" href="Style.css" type="text/css">
+    <title>Biological Background</title>
+</head>
+<body>
+<h1 style="color:white"><u>Biological Background </u> </h1>   <br>
+    <div id="out1">
+        <form action="Form2.php" target="_self" method="post">
+            <label for="DNA"> DNA</label>
+            <p> <b>DNA is a double helix formed by base pairs attached to a sugar-phosphate backbone.</b></p>
+            <p><b>The information in DNA is stored as a code made up of four chemical bases: adenine (A), guanine (G), cytosine (C), 
+            and thymine (T).</b></p>
+            <p><b> Human DNA consists of about 3 billion bases, and more than 99 percent of those bases are the same in all people.</b></p>
+            <p><b>An important property of DNA is that it can replicate, or make copies of itself,
+            Each strand of DNA in the double helix can serve as a pattern for duplicating the sequence of bases.</b></p>
+            <hr> <br>
+            <label > protein </label>
+            <br />
+              <p> <b>A protein is a naturally occurring, extremely complex substance that consists of amino acid residues joined by peptide bonds. 
+              </b></p>
+             <p><b>Proteins are present in all living organisms and include many essential biological compounds such as enzymes, hormones, and antibodies.</b></p>
+             <p><b> There are 20 different types of amino acids that can be combined to make a protein.</b></p>
+        </form>
+    </div>
+    <div id="out2">
+        <form action="Form2.php" target="_self" method="post">
+            <label > RNA </label>
+            <br />
+            <p> <b>RNA typically is a single-stranded biopolymer.</b></p>
+            <p><b> polymeric molecule essential in various biological roles in coding, decoding, regulation and expression of genes</b></p>
+            <p><b> Synthesis of RNA is usually catalyzed by an enzyme—RNA polymerase—using DNA as a template</b></p>
+            <p><b>An important property of DNA is t active processes is protein synthesis.</b></p>
+            <hr>
+            <label for="Gene"> Gene</label>
+             <p> <b>A gene is the basic physical and functional unit of heredity.</b></p>
+             <p><b>Genes are made up of DNA. Some genes act as instructions to make molecules called proteins</b></p>
+             <p><b>estimated that humans have between 20,000 and 25,000 genes.</b></p>
+             <p><b>Every person has two copies of each gene, one inherited from each parent. Most genes are the same in all people,
+                    but a small number of genes (less than 1 percent of the total) are slightly different between people.</b></p>
+            
+        </form>
+    </div>
+    <center>
+        <div id="out3">
+            <form action="Form2.php target=" _self" method="post">
+            <fieldset>
+                    <legend> <b>Functions</b></legend>
+                    <br>
+                <label for="F">base_Content:- </label>
+                <br />
+                <p><b>return is the percentage of Nucleotides in a DNA</b></p>     
+                <hr>
+                <label for="F">GC_Content :-</label>
+                 <p><b> return is the percentage of nitrogenous bases in a DNA or RNA molecule that are
+                 either guanine (G) or cytosine (C)</b></p>
+                  <hr>
+                   <label for="F">N_Content :-</label>
+                    <p><b> return is the percentage of Undefined bases in a DNA or RNA molecule </b></p>
+                 <hr>
+                 <label for="F">N_fiter :-</label>
+                    <p><b> return is the sequence with deleted Undefined Bases</b></p>
+                     <hr>
+                      <label for="F">complement:-</label>
+                    <p><b> return is the complement sequense as (A-T) (T-A) (G-C)(C-G)</b></p>
+                     <hr>
+                     <label for="F">Reverse_complement:-</label>
+                    <p><b> return is the Reverse seq after applying complement</b></p>
+                     <hr>
+                     <label for="F">Transcribe:-</label>
+                    <p><b> return RNA as (T-U)</b></p>
+                     <hr>
+                      <label for="F">Translate:-</label>
+                    <p><b> return Protein with codon table</b></p>
+            </form>
+        </div>
+    </center>
+         <form action="Form2.php" target="_self" method="post">    
+         <button type="submit" style="color: darkblue; width: 20%;height: 50px; float: right; font-size:large"  >Next</button>
+</body>
+
+</html>
